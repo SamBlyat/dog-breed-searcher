87873a00c326618c0863029e3d709e2e08cfd663
SamBlyat <69095296+SamBlyat@users.noreply.github.com>
2023-05-15 05:44:49 +0200

    MADE SEARCHING WORK!!! WOHOOOO!

--- script.js
+++ script.js
@@ -27,5 +27,69 @@ imagesData.map(function(imageData) {
 })
 
 function SearchDogs() {
-
+    while (document.getElementById("gallery").firstChild) {
+        document.getElementById("gallery").removeChild(document.getElementById("gallery").firstChild);
+    }
+    let breedName = searchField.value;
+  let searchURL = `https://api.thedogapi.com/v1/breeds/search?q=${breedName}`;
+  fetch(searchURL,{headers: {
+    'x-api-key': apiKey
+  }})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    if (data.length === 0) {
+      let error = document.createElement('p');
+      error.textContent = 'No results found';
+      document.getElementById('gallery').appendChild(error);
+      return;
+    }
+    let breedId = data[0].id;
+    let imagesURL = `https://api.thedogapi.com/v1/images/search?breed_id=${breedId}`;
+    fetch(imagesURL,{headers: {
+      'x-api-key': apiKey
+    }})
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      if (data.length === 0) {
+        let error = document.createElement('p');
+        error.textContent = 'No results found';
+        document.getElementById('gallery').appendChild(error);
+        return;
+      }
+      let imagesData = data;
+      imagesData.sort(function(a,b){
+        if(a.id < b.id) { return -1; }
+        if(a.id > b.id) { return 1; }
+        return 0;
+      });
+      let closestDog = imagesData[0];
+      let image = document.createElement('img');
+      image.id = "searchImg"
+      image.src = `${closestDog.url}`;
+      let gridCell = document.createElement('div');
+      gridCell.classList.add('srch-col');
+      gridCell.classList.add('srch-col-lg');
+      gridCell.appendChild(image)
+      document.getElementById('gallery').appendChild(gridCell);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 }
+
+function closestMatch(str, arr) {
+  return arr.reduce(function(prev, curr) {
+    return (Math.abs(curr.length - str.length) < Math.abs(prev.length - str.length) ? curr : prev);
+  });
+}
+
+document.querySelector('input').addEventListener('keydown', function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+    }
+});
